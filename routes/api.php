fae6f89be952beaa80184a69240678de71c19525
Aly Ossama <alyossama8@gmail.com>
2024-05-24 01:54:57 +0300

    Implemented routes for users and products

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Register a new user
+Route::post('users', [UserController::class, 'store'])->name('users.store');
+
+// User login
+Route::post('login', [UserController::class, 'login'])->name('users.login');
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    // Users
+    Route::resource('users', UserController::class)->except(['store']);
+
+    // Products
+    Route::resource('products', ProductController::class);
+});
